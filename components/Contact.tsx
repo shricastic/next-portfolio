386892d03d5166f24b762db3cdd5a9851816ef89
Shri <shricse07@gmail.com>
2024-10-10 03:16:51 +0530

    contact layout changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,68 +1,20 @@
 "use client";
 
-import React, {useState} from "react";
-import Socials from './Socials'
+import React from "react";
+import Socials from './Socials';
 
 export default function Contact() {
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log({ email, subject, message });
-  };
-
-
   return (
     <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl font-bold text-center text-white relative z-20 pb-10"> Contact Me</h1>
-      
-      <div className="flex w-1/2 justify-center">
-        <form onSubmit={handleSubmit} className="space-y-2 w-full md:w-1/2">
-          <div>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="rounded-lg p-3 w-full"
-              placeholder="shricse07@gmail.com"
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="subject"
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
-              className="rounded-lg p-3 w-full"
-              placeholder="Let us know how I can help you"
-              required
-            />
-          </div>
-          
-          <div className="sm:col-span-2"> 
-            <textarea
-              id="message"
-              rows={3}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="rounded-lg p-3 w-full"
-              placeholder="Leave a comment..."
-            ></textarea>
-          </div>
-
-          <button type="submit" className="rounded-lg bg-blue-500 p-2 w-full">
-            Send message
-          </button>
-        </form>
-      </div>  
-      
-      <div className="pt-20"> <Socials /> </div>
-
+      <div className="bg-[url('/images/contact.jpg')] bg-center bg-cover h-1/2 w-11/12 flex flex-col items-center sm:w-1/2 overflow-hidden hover:shadow-black shadow-[0px_0px_10px_0px_#6cd3eb] rounded-lg transition-transform duration-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] transform hover:scale-110">
+        <h1 className="font-bold text-6xl pt-10 pb-20  md:text-9xl bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-red-600">Let's talk!</h1> 
+        <div className='fit-content rounded-full bg-black'>
+          <h1 className="text-2xl p-2 pl-4 pr-4 md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-red-600">shricse07@gmail.com</h1>
+        </div>
+      </div>    
+  
+      <div className="pt-20"><Socials /></div>
     </div>
   );
 }
+
